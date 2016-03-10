793d7717f6ef8e6a63731cf4a49d0698e6ff8e7e
fiorenzo pizza <fiorenzino@gmail.com>
2016-03-10 08:19:06 +0100

    prezzo

--- app/services/prodotti-service.ts
+++ app/services/prodotti-service.ts
@@ -8,10 +8,12 @@ export class ProdottiService {
         console.log('prodotti service init');
         this.prodotti = [
             new Prodotto(
-                'Angular',
+                '123',
+                'prodotto1',
                 'angular',
                 'A powerful Javascript framework for building single page apps. Angular is open source, and maintained by Google.',
-                '#E63135'
+                '#E63135',
+                123.00
             )
         ]
     }
