4e3f0763d4fd1fdde6ab39630603776c90b680c2
fiorenzo pizza <fiorenzino@gmail.com>
2016-03-14 17:34:42 +0100

    incr

--- app/model/prodotto-ordine.ts
+++ app/model/prodotto-ordine.ts
@@ -5,18 +5,18 @@ export class ProdottoOrdine {
 
     constructor(public prodotto:Prodotto) {
         this.quantita = 1;
-        this.parziale = (this.quantita*this.prodotto.price);
-    }
-
-    decr() {
-        this.quantita = this.quantita + 1;
-        this.parziale = (this.quantita*this.prodotto.price);
+        this.parziale = (this.quantita * this.prodotto.price);
     }
 
     incr() {
+        this.quantita = this.quantita + 1;
+        this.parziale = (this.quantita * this.prodotto.price);
+    }
+
+    decr() {
         if (this.quantita > 1) {
             this.quantita = this.quantita - 1;
-            this.parziale = (this.quantita*this.prodotto.price);
+            this.parziale = (this.quantita * this.prodotto.price);
         }
     }
 
