e647c190bf0a4fb36f77fa3134677ce8f9d527c1
fiorenzo pizza <fiorenzino@gmail.com>
2016-03-14 16:38:46 +0100

    merdina

--- app/pages/prodotti/prodotto-view.ts
+++ app/pages/prodotti/prodotto-view.ts
@@ -2,7 +2,7 @@
 import {NavController, NavParams} from 'ionic-angular';
 import {Page} from 'ionic-angular';
 import {forwardRef} from 'angular2/core';
-import {AddProdotto} from "../ordini/addProdotto";
+import {AddProdotto} from "../ordini/add-prodotto";
 
 @Page({
     templateUrl: 'build/pages/prodotti/prodotto-view.html',
